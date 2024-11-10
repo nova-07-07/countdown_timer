79bf9fe4843bc786fcc3199c137117e2065c8618
nova-07-07 <nova234607ns@gmail.com>
2024-11-10 15:33:52 +0530

    update code js and css

--- indexs.js
+++ indexs.js
@@ -8,65 +8,62 @@ const reset = document.querySelector('.reset');
 
 let count;
 
-start.addEventListener('click',()=>{
-        stop.style.display = "initial";
-        start.style.display = "none"
-    if(hour.value == "00" && min.value == "00" && sec.value == "00"){
-        return ;
+start.addEventListener('click', () => {
+    stop.style.display = "initial";
+    start.style.display = "none";
+
+    // Ensure the timer starts only if all values are not zero
+    if (hour.value === "00" && min.value === "00" && sec.value === "00") {
+        return;
     }
+
     count = setInterval(() => {
         time();
     }, 1000);
+});
 
-       function time(){
-        
-        if(sec.value > 60){
-            min.value++;
-        sec.value = sec.value - 59;
-     }
-        if(min.value > 60){
-            hour.value++;
-        min.value = min.value - 60;
+function time() {
+    let h = parseInt(hour.value, 10);
+    let m = parseInt(min.value, 10);
+    let s = parseInt(sec.value, 10);
+
+    // Handle seconds
+    if (s > 0) {
+        s--;
+    } else if (m > 0) {
+        m--;
+        s = 59;
+    } else if (h > 0) {
+        h--;
+        m = 59;
+        s = 59;
     }
-        if(hour.value == 0 && min.value == 0 && sec.value == 0){
-        hour.value = "";
-        min.value = "";
-        sec.value = "";
-        stopInterval()
-        }
-   
-       if(sec.value != 0){
-            sec.value = `${sec.value <= 10 ? "0":""}${sec.value -1}`;
-        }
-       if( min.value !=0 && sec.value == 0 ){
-            sec.value = 59 ;
-            min.value = `${min.value <= 10 ? "0":""}${min.value -1}`;
-        }
-       if( hour.value !=0 && min.value == 0 ){
-            min.value = 59 ;
-            sec.value = 59 ;
-            hour.value = `${hour.value <= 10 ? "0":""}${hour.value -1}`;
-        }
-      
-    
-}})
-function stopInterval(main){
-    start.innerHTML = main == 'pause' ? "countinue":"start";
-    stop.style.display = "none";
-    start.style.display = "initial"
-    clearInterval(count);
-    
-}
-stop.addEventListener('click',()=>{
-    stopInterval('pause');
-    
-    })
 
-    reset.addEventListener('click',()=>{
-        hour.value = ""
-        min.value = ""
-        sec.value = ""
+    // Update the input fields with leading zeroes
+    hour.value = h < 10 ? `0${h}` : `${h}`;
+    min.value = m < 10 ? `0${m}` : `${m}`;
+    sec.value = s < 10 ? `0${s}` : `${s}`;
+
+    // Stop the interval when time reaches zero
+    if (h === 0 && m === 0 && s === 0) {
         stopInterval();
-})
+    }
+}
 
+function stopInterval() {
+    start.innerHTML = "Start";
+    stop.style.display = "none";
+    start.style.display = "initial";
+    clearInterval(count);
+}
 
+stop.addEventListener('click', () => {
+    stopInterval();
+});
+
+reset.addEventListener('click', () => {
+    hour.value = "00";
+    min.value = "00";
+    sec.value = "00";
+    stopInterval();
+});
